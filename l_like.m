bc0035dbabe41dbe5e012e3c32b0f256e92f792e
LL6425 <121629100+LL6425@users.noreply.github.com>
2023-01-10 00:01:00 +0100

    Update l_like.m

--- l_like.m
+++ l_like.m
@@ -1,10 +1,9 @@
 function [loglik] = l_like(parameters,data)
 
-% this function does compute the log-likelihood of lambda, based on the
+% Function computing the log-likelihood of lambda, based on the
 % likelihood of a Student t distribution where the location is constant
 % and the scale is an exponential function of a time varying parameter
-% lambda_t whose evolutoion (along with the likelihood function) is
-% contained in the file filter_scale.m 
+
 
 phi = parameters(1);
 nu = parameters(2); 
